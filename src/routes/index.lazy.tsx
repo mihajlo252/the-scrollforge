904c99cb171ecc7a3553b1e874cc96e9d4ea1aad
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-11-02 19:48:41 +0100

    Add styles for form and welcome

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -6,7 +6,6 @@ import { useEffect, useState } from "react";
 import { useUserStore } from "../zustand/stores";
 import { useCharacterStore } from "../zustand/stores";
 
-
 export const Route = createLazyFileRoute("/")({
     component: signinScreen,
 });
@@ -40,27 +39,51 @@ function signinScreen() {
 
     if (currentUser) {
         return (
-            <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="overflow-hidden">
-                <BoxSection styles="w-full p-5 flex-col text-start gap-2 overflow-y-scroll pt-0">
-                   <h1>Welcome {JSON.parse(currentUser).user.email}</h1>
-                   <Link to="/character-profile">Enter</Link>
+            <motion.main
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className="flex h-full flex-col items-center justify-center overflow-hidden"
+            >
+                <BoxSection styles="w-full p-5 flex-col text-start gap-2 overflow-y-scroll pt-0 justify-center items-center gap-20">
+                    <h1 className="text-5xl">Welcome {JSON.parse(currentUser).user.email}</h1>
+                    <Link to="/character-profile" className="btn btn-primary text-2xl">
+                        Enter
+                    </Link>
                 </BoxSection>
             </motion.main>
         );
     }
 
     return (
-        <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="overflow-hidden">
-            <BoxSection styles="w-full p-5 flex-col text-start gap-2 overflow-y-scroll pt-0">
-                <form onSubmit={(e) => login(e)}>
-                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button onClick={login}>Login</button>
+        <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="h-full overflow-hidden">
+            <BoxSection styles="w-full p-5 flex-col text-start overflow-y-scroll pt-0 justify-center items-center">
+                <form
+                    onSubmit={(e) => login(e)}
+                    className="flex w-full flex-col items-center justify-center gap-5 overflow-y-scroll p-5 pt-0 text-start"
+                >
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="text-neutral-content"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="pass">Password:</label>
+                        <input
+                            type="password"
+                            placeholder="*********"
+                            id="pass"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="text-neutral-content"
+                        />
+                    </div>
+                    <button onClick={login} className="btn btn-primary">Login</button>
                 </form>
             </BoxSection>
         </motion.main>
