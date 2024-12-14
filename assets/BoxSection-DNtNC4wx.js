dd08103f8ca3ca36c8d1469959c1edc72731b3bb
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-12-14 15:50:54 +0100

    Updates

---
+++ assets/BoxSection-DNtNC4wx.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Bny84ZkB.js";const t=({styles:e,children:r})=>o.jsx("div",{className:`flex h-full rounded-lg border-2 border-base-300 bg-base-300 text-neutral ${e}`,children:r});export{t as B};
