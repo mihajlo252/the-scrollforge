d2258cd5e31f03057992b90b042c7de7d7bc110b
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-12-21 00:06:38 +0100

    Updates

--- assets/getUserFromLocal-B5L4l1Lr.js
+++
@@ -1,1 +0,0 @@
-const e=()=>JSON.parse(JSON.stringify(localStorage.getItem("sb-iyfoqgbhaxcedpmuvfkr-auth-token")));export{e as g};
--- assets/sendData-Cmy9Fvbp.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-D2BXpyy2.js";const c=async(r,s,t)=>{try{const{error:a}=await e.from(r).update(t).eq("id",s);if(a)throw a.message}catch(a){throw a}};export{c as s};
