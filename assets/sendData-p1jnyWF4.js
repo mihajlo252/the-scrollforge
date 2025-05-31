464d7decbcff6b6fb39578b38ae43fe6da4d665c
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2025-05-31 19:14:36 +0200

    Updates

--- assets/sendData-p1jnyWF4.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-Jan0dvxk.js";const c=async(o,s,a)=>{try{const{error:r}=await e.from(o).update(a).eq("id",s);r&&console.error(r)}catch(r){console.error(r)}};export{c as s};
