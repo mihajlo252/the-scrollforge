e93993cb2ada3852b2dc5507e3820818c6c2f9f8
mihajlo252 <mihajlo.kostic25200@gmail.com>
2025-07-24 16:39:51 +0200

    Updates

---
+++ assets/GM-DHSvhT4Y.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Cdihcqf2.js";const a=({className:t,children:s})=>e.jsx("div",{className:`text-accent ${t}`,children:s});export{a as G};
--- assets/GM-rBoraxKJ.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-ElyLOzAk.js";const a=({className:t,children:s})=>e.jsx("p",{className:`text-accent ${t}`,children:s});export{a as G};
