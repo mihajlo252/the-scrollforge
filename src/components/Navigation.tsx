55cc59ca98eef0f9646688b9926a2fe736afb90f
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2025-09-01 23:09:09 +0200

    Add signOut popup question

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Link, useNavigate } from "@tanstack/react-router";
 import { signOut } from "../utilities/signOut";
 import { getUserFromLocal } from "../utilities/getUserFromLocal";
@@ -7,83 +7,105 @@ import { useUserStore } from "../zustand/stores";
 import { BorderButton } from "./BorderButton";
 import logo from "/assets/the-scrollforge-logo.png";
 import { toast } from "../utilities/toasterSonner";
+import { Popup } from "./Popup";
 
 export const Navigation = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const { user, removeUser } = useUserStore();
-    const [sign, setSign] = useState("Sign Up");
+  const { user, removeUser } = useUserStore();
+  const [sign, setSign] = useState("Sign Up");
+  const [openSignOut, setOpenSignOut] = useState(false);
 
-    const [newUser, setNewUser] = useState(getUserFromLocal() || "");
+  const [newUser, setNewUser] = useState(getUserFromLocal() || "");
 
-    const handleSignOut = async () => {
-        await signOut();
-        removeUser();
-        setNewUser("");
-        navigate({ to: "/" });
+  const handleSignOut = async () => {
+    await signOut();
+    setOpenSignOut(false)
+    removeUser();
+    setNewUser("");
+    navigate({ to: "/" });
+  };
+  const handleRedirect = (path: string) => {
+    if (path === "/character") {
+      let character = JSON.parse(JSON.parse(JSON.stringify(localStorage.getItem("character")))) || null;
+      if (!character) {
+        toast({ style: "bg-secondary text-white", message: "Please select a character" });
+        return;
+      }
+    }
+    if (path === "/signup") {
+      setSign("Sign In");
+      navigate({ to: "/signup" });
+    }
+    if (path === "/") {
+      setSign("Sign Up");
+      navigate({ to: "/" });
+    }
+    navigate({ to: path });
+  };
+
+  useEffect(() => {
+    if (user) {
+      setNewUser(user);
+    }
+  }, [user]);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") handleRedirect("/character");
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
     };
-    const handleRedirect = (path: string) => {
-        if (path === "/character") {
-            let character = JSON.parse(JSON.parse(JSON.stringify(localStorage.getItem("character")))) || null;
-            if (!character) {
-                toast({ style: "bg-secondary text-white", message: "Please select a character" });
-                return;
-            }
-        }
-        if (path === "/signup") {
-            setSign("Sign In");
-            navigate({ to: "/signup" });
-        }
-        if (path === "/") {
-            setSign("Sign Up");
-            navigate({ to: "/" });
-        }
-        navigate({ to: path });
-    };
+  }, []);
 
-    useEffect(() => {
-        if (user) {
-            setNewUser(user);
-        }
-    }, [user]);
+  return (
+    <motion.nav className="flex justify-between gap-2" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <Link to={newUser ? "/profile" : "/"} className="font-bold uppercase text-neutral no-underline">
+        <img src={logo} className="w-28" alt="Dash&Play Logo" />
+      </Link>
+      {!newUser && (
+        <BorderButton
+          text={sign === "Sign Up" ? "Sign Up" : "Sign In"}
+          style="border-primary text-primary hover:border-primary hover:bg-primary"
+          event={() => handleRedirect(sign === "Sign Up" ? "/signup" : "/")}
+        />
+      )}
 
-    return (
-        <motion.nav className="flex justify-between gap-2" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-            <Link to={newUser ? "/profile" : "/"} className="font-bold uppercase text-neutral no-underline">
-                <img src={logo} className="w-28" alt="Dash&Play Logo" />
-            </Link>
-            {!newUser && (
-                <BorderButton
-                    text={sign === "Sign Up" ? "Sign Up" : "Sign In"}
-                    style="border-primary text-primary hover:border-primary hover:bg-primary"
-                    event={() => handleRedirect(sign === "Sign Up" ? "/signup" : "/")}
-                />
-            )}
-
-            {newUser && (
-                <div className="flex items-center gap-2">
-                    <BorderButton
-                        text="Daggerheart Demo"
-                        style="border-accent text-accent hover:border-accent hover:bg-accent"
-                        event={() => handleRedirect("/daggerheart-demo/acts")}
-                    />
-                    <BorderButton
-                        text="Character"
-                        style="border-primary text-primary hover:border-primary hover:bg-primary"
-                        event={() => handleRedirect("/character")}
-                    />
-                    <BorderButton
-                        text="Profile"
-                        style="border-primary text-primary hover:border-primary hover:bg-primary"
-                        event={() => handleRedirect("/profile")}
-                    />
-                    <BorderButton
-                        text="Sign Out"
-                        style="border-secondary text-secondary hover:border-secondary hover:bg-secondary"
-                        event={handleSignOut}
-                    />
+      {newUser && (
+        <div className="flex items-center gap-2">
+          <BorderButton
+            text="Daggerheart Demo"
+            style="border-accent text-accent hover:border-accent hover:bg-accent"
+            event={() => handleRedirect("/daggerheart-demo/acts")}
+          />
+          <BorderButton
+            text="Character"
+            style="border-primary text-primary hover:border-primary hover:bg-primary"
+            event={() => handleRedirect("/character")}
+          />
+          <BorderButton
+            text="Profile"
+            style="border-primary text-primary hover:border-primary hover:bg-primary"
+            event={() => handleRedirect("/profile")}
+          />
+          <BorderButton text="Sign Out" style="border-secondary text-secondary hover:border-secondary hover:bg-secondary" event={() => setOpenSignOut(true)} />
+          <AnimatePresence>
+            {openSignOut && (
+              <Popup closerFunc={setOpenSignOut}>
+                <p>Are you sure you want to sign out?</p>
+                <div className="flex gap-2">
+                  <BorderButton text="Yes" style="border-primary text-primary hover:border-primary hover:bg-primary" event={handleSignOut} />
+                  <BorderButton text="No" style="border-secondary text-secondary hover:border-secondary hover:bg-secondary" event={() => setOpenSignOut(false)} />
                 </div>
+              </Popup>
             )}
-        </motion.nav>
-    );
+          </AnimatePresence>
+        </div>
+      )}
+    </motion.nav>
+  );
 };
