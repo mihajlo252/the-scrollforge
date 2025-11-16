2584c534836096cbd3cacaa8776262635a95c1e8
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2025-11-16 17:09:17 +0100

    Refactor

--- src/components/BoxSection.tsx
+++ src/components/BoxSection.tsx
@@ -1,6 +1,6 @@
 export const BoxSection = ({styles, children}: {styles: string, children: React.ReactNode}) => {
   return (
-    <div className={`flex rounded-lg border-2 bg-opacity-70 border-slate-900 bg-base-300 text-neutral ${styles}`}>
+    <div className={`flex rounded-lg h-full border-2 bg-opacity-70 border-slate-900 bg-base-300 text-neutral ${styles}`}>
       {children}
     </div>
   )
