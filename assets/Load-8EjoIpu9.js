3fbc8a923687df9fc944d559aab865185a4fb91b
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2026-06-28 20:23:23 +0200

    Updates

--- assets/Load-8EjoIpu9.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-9TL5gGSS.js";const r=()=>s.jsx("div",{className:"loader"});export{r as L};
---
+++ assets/Load-mfNSapnn.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-C1TgYEQx.js";const r=()=>s.jsx("div",{className:"loader"});export{r as L};
